b080c40dd394bd2c3ede4c8008fc753a1595790e
Shawn Yeager <hello@shawnyeager.com>
2025-02-11 09:14:37 -0600

    changes to init zoxide last as required

--- bash/.bashrc
+++ bash/.bashrc
@@ -1,7 +1,7 @@
 # Exit if not interactive
 [[ $- != *i* ]] && return
 
-# Source all scripts from ~/.config/bash
+# Source all scripts from ~/.config/bash in natural alphabetical order
 for file in ~/.config/bash/*.sh; do
   [ -r "$file" ] && source "$file"
 done
--- bash/.config/bash/zz_tools.sh
+++ bash/.config/bash/zz_tools.sh
@@ -3,13 +3,13 @@ if command -v mise &>/dev/null; then
   eval "$(mise activate bash)"
 fi
 
-# Zoxide (smart `cd` alternative)
-if command -v zoxide &>/dev/null; then
-  eval "$(zoxide init bash)"
-fi
-
 # FZF (fuzzy finder)
 if command -v fzf &>/dev/null; then
   source /usr/share/fzf/completion.bash
   source /usr/share/fzf/key-bindings.bash
+fi
+
+# Zoxide (smart `cd` alternative); moved to end to ensure it's initialized last
+if command -v zoxide &>/dev/null; then
+  eval "$(zoxide init bash)"
 fi
